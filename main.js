93ee00ae2765085f101240ce24564ebadae2acdf
Saashvath <74235537+Saashvath@users.noreply.github.com>
2021-07-26 17:10:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload(){
+
+
+}
+
+function setup(){
+
+canvas = createCanvas(400,400)
+canvas.center;
+
+}
+
+function draw(){
+
+
+}
+
+function take_picture(){
+
+save("birthday_picture.png")
+}
